c20c1e1bf43fb8c8aad465f9d9ec8aa0739dc9da
Jörg Dietrich <astro@joergdietrich.com>
2014-01-26 21:54:47 +0100

    consistent function naming, pep8 clean-up

--- NFW.py
+++ NFW.py
@@ -17,10 +17,11 @@ def arcsec(z):
     val = 1.j * np.log(val2 + val1)
     return 0.5 * np.pi + val
 
+
 class InvalidNFWValue(Exception):
     pass
-    
-    
+
+
 class NFW(object):
     """Compute properties of an NFW halo.
 
@@ -35,10 +36,11 @@ class NFW(object):
     cosmology - "wmap7" or an instance of the Cosmology class
     size_type - "(radius|mass)" specifies whether the halo size is given as
                 radius or mass
-    overdensity - the factor above the critical/mean density of the Universe                      at which mass/radius are computed. Default 200
+    overdensity - the factor above the critical/mean density of the Universe
+                  at which mass/radius are computed. Default 200
     overdensity_type = "(critical|mean)"
     """
-                 
+
     def __init__(self, size, c, z, cosmology="wmap7", size_type="mass",
                  overdensity=200.,
                  overdensity_type="critical"):
@@ -46,41 +48,41 @@ class NFW(object):
             self.cosmo = Cosmology(0.272, 0.728, 0.704)
         else:
             self.cosmo = cosmology
-            
+
         if overdensity_type != "critical":
             print "You must be kidding."
             print "Of course overdensities are defined wrt the " \
-            "critical density."
-            
+                "critical density."
+
         self.c = float(c)
         self.z = float(z)
 
         self.rho_c = 3. * (100 * self.cosmo.hubble(self.z))**2 / \
-                     (8. * np.pi * scipy.constants.G)
+            (8. * np.pi * scipy.constants.G)
         self.rho_c *= 1e12 * scipy.constants.parsec / 1.98892e30
-        self.delta_c = 200. / 3. * self.c**3 / (np.log(1. + c) - c /(1. + c))
-        
+        self.delta_c = 200. / 3. * self.c**3 / (np.log(1. + c) - c / (1. + c))
+
         if size_type == "mass":
             r_Delta = (3. * size /
-                   (4. * np.pi * overdensity * self.rho_c))**(1./3.)
+                       (4. * np.pi * overdensity * self.rho_c))**(1./3.)
         elif size_type == "radius":
             r_Delta = float(size)
         else:
             raise InvalidNFWValue(size_type)
-        self.r_s= self.rDelta2rs(r_Delta, overdensity)
+        self.r_s = self._rDelta2rs(r_Delta, overdensity)
         return
 
-    def _conversion(self, rs, r_Delta, overdensity):
-        rs = overdensity  / 3. * r_Delta**3 - \
+    def _rDelta2r200_zero(self, rs, r_Delta, overdensity):
+        z = overdensity / 3. * r_Delta**3 - \
             self.delta_c * rs**3 * \
             (math.log((rs + r_Delta) / rs) - r_Delta / (rs + r_Delta))
-        return rs
+        return z
 
-    def rDelta2rs(self, r_Delta, overdensity):
-        r200 = opt.newton(self._conversion, r_Delta, 
+    def _rDelta2rs(self, r_Delta, overdensity):
+        r200 = opt.brentq(self._rDelta2r200_zero, 1e-6, 100,
                           args=(r_Delta, overdensity))
         return r200
-        
+
     def __str__(self):
         prop_str = "NFW halo with concentration %.2g at redshift %.2f:\n\n" \
                    % (self.c, self.z,)
@@ -92,12 +94,12 @@ class NFW(object):
 
     def _mean_density_zero(self, r, Delta):
         return self.mean_density(r) - Delta * self.rho_c
-    
+
     def radius_Delta(self, Delta):
         """Find the radius at which the mean density is Delta times the
         critical density. Returns radius in Mpc."""
         x0 = opt.brentq(self._mean_density_zero, 1e-6, 10,
-                                  args=(Delta,))
+                        args=(Delta,))
         return x0
 
     def mass_Delta(self, Delta):
@@ -105,27 +107,26 @@ class NFW(object):
         is Delta times the critical density. Returns mass in M_sun."""
         r = self.radius_Delta(Delta)
         return self.mass(r)
-        
+
     def density(self, r):
         """Compute the density rho of an NFW halo at radius r (in Mpc)
         from the center of the halo. Returns M_sun/Mpc^3."""
-        return self.rho_c * self.delta_c / (r / self.r_s * \
-                                                (1 + r / self.r_s)**2)
+        return self.rho_c * self.delta_c / (r / self.r_s *
+                                            (1 + r / self.r_s)**2)
 
     def mean_density(self, r):
         """Compute the mean density inside a radius r (in
         Mpc). Returns M_sun/Mpc^3."""
-        return 3. * (self.r_s / r) **3 * self.delta_c * self.rho_c * \
-               (np.log((1 + r / self.r_s)) - (r / self.r_s) / \
-                    (1 + r / self.r_s))
+        return 3. * (self.r_s / r)**3 * self.delta_c * self.rho_c * \
+            (np.log((1 + r / self.r_s)) - (r / self.r_s) /
+             (1 + r / self.r_s))
 
     def mass(self, r):
         """Compute the mass of an NFW halo inside radius r (in Mpc)
         from the center of the halo. Returns mass in M_sun."""
         return 4. * np.pi * self.delta_c * self.rho_c * self.r_s**3 * \
-               (np.log((1 + r / self.r_s)) - (r / self.r_s) / \
-                (1 + r / self.r_s))
-
+            (np.log((1 + r / self.r_s)) - (r / self.r_s) /
+             (1 + r / self.r_s))
 
     def sigma(self, r):
         """Compute the surface mass density of the halo at distance r
@@ -135,20 +136,19 @@ class NFW(object):
         #    (np.log(1. + self.c) - self.c  / (1. + self.c))
         val1 = 1. / (x**2 - 1.)
         val2 = arcsec(x) / (sm.sqrt(x**2 - 1.))**3
-        return float(2. * self.r_s * self.rho_c * self.delta_c * \
-                         (val1 - val2)).real
+        return float(2. * self.r_s * self.rho_c * self.delta_c *
+                     (val1 - val2)).real
 
     def delta_sigma(self, r):
         """Compute the Delta surface mass density of the halo at
         radius r (in Mpc) from the halo center."""
         x = r / self.r_s
         delta_c = 200 / 3. * self.c**3 / \
-            (np.log(1. + self.c) - self.c  / (1. + self.c))
+            (np.log(1. + self.c) - self.c / (1. + self.c))
         fac = 2. * self.r_s * self.rho_c * delta_c
         val1 = 1. / (1. - x**2)
-        num = (3. * x**2 - 2) * arcsec(x) 
+        num = (3. * x**2 - 2) * arcsec(x)
         div = x**2 * (sm.sqrt(x**2 - 1.))**3
         val2 = num / div
         val3 = 2. * np.log(x / 2.) / x**2
         return (fac * (val1 + val2 + val3)).real
-        
