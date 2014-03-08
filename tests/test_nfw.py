0c6a35e42a997cd14b1ec1efbef0f8915025b3c7
Jörg Dietrich <astro@joergdietrich.com>
2014-03-08 14:54:57 +0100

    Update desired values with hopefully more precise new ones

--- tests/test_nfw.py
+++ tests/test_nfw.py
@@ -23,7 +23,7 @@ class TestNFW(TestCase):
         nfw = NFW(m200, c, z)
         assert_equal(nfw.c, c)
         assert_equal(nfw.z, z)
-        assert_almost_equal(nfw.r_s.value, 0.374162, 3)
+        assert_almost_equal(nfw.r_s.value, 0.3724844259709579, 3)
 
     def test_radius_Delta(self):
         m200 = 1e15
@@ -31,11 +31,11 @@ class TestNFW(TestCase):
         z = 0.3
         nfw = NFW(m200, c, z)
         r200 = nfw.radius_Delta(200)
-        assert_almost_equal(r200.value, 1.870811, 6)
+        assert_almost_equal(r200.value, 1.8624221298548365, 6)
         r500 = nfw.radius_Delta(500)
-        assert_almost_equal(r500.value, 1.236665, 6)
+        assert_almost_equal(r500.value, 1.231119031798481, 6)
         r2500 = nfw.radius_Delta(2500)
-        assert_almost_equal(r2500.value, 0.554511, 6)
+        assert_almost_equal(r2500.value, 0.5520242539181, 6)
 
     def test_mass_Delta(self):
         m200 = 1e15
@@ -51,7 +51,7 @@ class TestNFW(TestCase):
         z = 0.3
         nfw = NFW(m200, c, z)
         rho = nfw.density(1.23)
-        assert_almost_equal(rho.value/1e13, 2.623190, 6)
+        assert_almost_equal(rho.value/1e13, 2.628686177054833, 6)
 
     def test_mean_density(self):
         m200 = 1e15
@@ -59,7 +59,7 @@ class TestNFW(TestCase):
         z = 0.3
         nfw = NFW(m200, c, z)
         rho = nfw.mean_density(1.23)
-        assert_almost_equal(rho.value/1e13, 9.221492, 6)
+        assert_almost_equal(rho.value/1e13, 9.256897197704966, 6)
 
     def test_mess(self):
         m200 = 1e15
@@ -67,7 +67,7 @@ class TestNFW(TestCase):
         z = 0.3
         nfw = NFW(m200, c, z)
         m = nfw.mass(1.32)
-        assert_almost_equal(m.value/1e14, 7.6282136906654214, 6)
+        assert_almost_equal(m.value/1e14, 7.656709240756399, 6)
         
     def test_sigma(self):
         m200 = 1e15
@@ -75,7 +75,7 @@ class TestNFW(TestCase):
         z = 0.3
         nfw = NFW(m200, c, z)
         s = nfw.sigma(1.12)
-        assert_almost_equal(s.value/1e13, 8.403954902419617, 6)
+        assert_almost_equal(s.value/1e13, 8.418908648577666, 6)
 
     def test_delta_sigma(self):
         m200 = 1e15
@@ -83,7 +83,7 @@ class TestNFW(TestCase):
         z = 0.3
         nfw = NFW(m200, c, z)
         ds = nfw.delta_sigma(1.12)
-        assert_almost_equal(ds.value/1e14, 1.381662, 6)
+        assert_almost_equal(ds.value/1e14, 1.3877272300533743, 6)
         
     def test_mass_consistency(self):
         m200 = 1e15
