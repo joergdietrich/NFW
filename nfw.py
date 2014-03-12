3a1638f0d20cf6887d945f6f9c887fa77565f4a6
Jörg Dietrich <astro@joergdietrich.com>
2014-03-12 12:47:42 +0100

    make equations more beautiful, better input unit checking

--- nfw.py
+++ nfw.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+from __future__ import division
+
 import math
 
 import numpy as np
@@ -119,8 +121,7 @@ class NFW(object):
     def delta_c(self):
         """Characterstic overdensity
         """
-        return 200. / 3. * self.c**3 \
-            / (np.log(1. + self.c) - self.c / (1. + self.c))
+        return 200/3 * self.c**3 / (np.log(1 + self.c) - self.c/(1. + self.c))
 
     @property
     def rho_c(self):
@@ -144,9 +145,9 @@ class NFW(object):
         if self._size_type == "mass":
             if self._update_required(self._rho_c):
                 self._rho_c = self.rho_c
-            self._r_Delta = (3. * self._size
-                             / (4. * np.pi * self._overdensity
-                                * self._rho_c))**(1./3.)
+            self._r_Delta = (3*self._size
+                             / (4 * np.pi * self._overdensity
+                                * self._rho_c))**(1/3)
         else:
             self._r_Delta = self._size
         return self._r_Delta
@@ -164,8 +165,8 @@ class NFW(object):
 
     def _rDelta2r200_zero(self, rs, r_Delta, overdensity):
         rs *= u.megaparsec
-        z = overdensity / 3. * r_Delta**3 - self.delta_c * rs**3 \
-            * (math.log((rs + r_Delta) / rs) - r_Delta / (rs + r_Delta))
+        z = (overdensity/3 * r_Delta**3) - (self.delta_c * rs**3) \
+            * (math.log((rs+r_Delta) / rs) - (r_Delta / (rs+r_Delta)))
         return z.value
 
     def _rDelta2rs(self, r_Delta, overdensity):
@@ -183,13 +184,12 @@ class NFW(object):
         return prop_str
 
     def _mean_density_zero(self, r, Delta):
-        return (self.mean_density(r) - Delta * self.rho_c).value
+        return (self.mean_density(r) - Delta*self.rho_c).value
 
     def radius_Delta(self, Delta):
         """Find the radius at which the mean density is Delta times the
         critical density. Returns radius in Mpc."""
-        x0 = opt.brentq(self._mean_density_zero, 1e-6, 10,
-                        args=(Delta,))
+        x0 = opt.brentq(self._mean_density_zero, 1e-6, 10, args=(Delta,))
         return x0 * u.megaparsec
 
     def mass_Delta(self, Delta):
@@ -201,51 +201,44 @@ class NFW(object):
     def density(self, r):
         """Compute the density rho of an NFW halo at radius r (in Mpc)
         from the center of the halo. Returns M_sun/Mpc^3."""
-        if not isinstance(r, u.Quantity):
-            r *= u.megaparsec
-        return self.rho_c * self.delta_c \
-            / (r / self.r_s * (1 + r / self.r_s)**2)
+        r = unit_checker(r, u.megaparsec)
+        x = r / self.r_s
+        return self.rho_c * self.delta_c/(x * (1+x)**2)
 
     def mean_density(self, r):
         """Compute the mean density inside a radius r (in
         Mpc). Returns M_sun/Mpc^3."""
-        if not isinstance(r, u.Quantity):
-            r *= u.megaparsec
-        return 3. * (self.r_s / r)**3 * self.delta_c * self.rho_c \
-            * (np.log((1 + r / self.r_s))
-               - (r / self.r_s) / (1 + r / self.r_s))
+        r = unit_checker(r, u.megaparsec)
+        x = r / self.r_s
+        return 3 * (1/x)**3 * self.delta_c * self.rho_c \
+            * (np.log((1 + x)) - x/(1 + x))
 
     def mass(self, r):
         """Compute the mass of an NFW halo inside radius r (in Mpc)
         from the center of the halo. Returns mass in M_sun."""
-        if not isinstance(r, u.Quantity):
-            r *= u.megaparsec
-        return 4. * np.pi * self.delta_c * self.rho_c * self.r_s**3 \
-            * (np.log((1 + r / self.r_s))
-               - (r / self.r_s) / (1 + r / self.r_s))
+        r = unit_checker(r, u.megaparsec)
+        x = r / self.r_s
+        return 4*np.pi * self.delta_c * self.rho_c * self.r_s**3 \
+            * (np.log((1 + x)) - x/(1 + x))
 
     def sigma(self, r):
         """Compute the surface mass density of the halo at distance r
         (in Mpc) from the halo center."""
-        if not isinstance(r, u.Quantity):
-            r *= u.megaparsec
+        r = unit_checker(r, u.megaparsec)
         x = r / self.r_s
-        val1 = 1. / (x**2 - 1.)
-        val2 = float((arcsec(x) / (sm.sqrt(x**2 - 1.))**3).real)
-        return 2. * self.r_s * self.rho_c * self.delta_c * (val1 - val2)
+        val1 = 1 / (x**2 - 1)
+        val2 = float((arcsec(x) / (sm.sqrt(x**2 - 1))**3).real)
+        return 2 * self.r_s * self.rho_c * self.delta_c * (val1-val2)
 
     def delta_sigma(self, r):
         """Compute the Delta surface mass density of the halo at
         radius r (in Mpc) from the halo center."""
-        if not isinstance(r, u.Quantity):
-            r *= u.megaparsec
+        r = unit_checker(r, u.megaparsec)
         x = r / self.r_s
-        delta_c = 200 / 3. * self.c**3 / \
-            (np.log(1. + self.c) - self.c / (1. + self.c))
-        fac = 2. * self.r_s * self.rho_c * delta_c
-        val1 = 1. / (1. - x**2)
-        num = (3. * x**2 - 2) * arcsec(x)
-        div = x**2 * (sm.sqrt(x**2 - 1.))**3
+        fac = 2 * self.r_s * self.rho_c * self.delta_c
+        val1 = 1 / (1 - x**2)
+        num = ((3 * x**2) - 2) * arcsec(x)
+        div = x**2 * (sm.sqrt(x**2 - 1))**3
         val2 = float((num / div).real)
-        val3 = 2. * np.log(x / 2.) / x**2
-        return fac * (val1 + val2 + val3)
+        val3 = 2 * np.log(x / 2) / x**2
+        return fac * (val1+val2+val3)
