b170e2bf202a3fcba544f21292a85c7dd5c4e2ec
Jörg Dietrich <astro@joergdietrich.com>
2016-03-04 11:59:59 +0100

    tests for mdelta_to_mdelta

--- tests/test_mass_concentration.py
+++ tests/test_mass_concentration.py
@@ -6,8 +6,11 @@ from numpy.testing.decorators import knownfailureif
 
 import astropy.cosmology
 from astropy import units as u
+from astropy.tests.helper import quantity_allclose
 
 import mass_concentration
+from nfw import NFW
+
 
 class TestMc(TestCase):
     @classmethod
@@ -51,3 +54,42 @@ class TestMc(TestCase):
                                                    self._cosmo)
         assert(isinstance(c, float))
         
+    def test_mdelta_to_mdelta(self):
+        overdensity = 50, 100, 199, 200, 500, 2500
+        m_in = 1e9, 1e13, 1e14, 1e15, 1e16
+        z = 0, 0.5, 1
+        func = mass_concentration.dolag_concentration
+        # common cases:
+        mdelta = mass_concentration.mdelta_to_mdelta(5e14, func, 200, 500,
+                                                     (0.3, self._cosmo))
+        assert(quantity_allclose(mdelta,
+                                 u.Quantity(397021380489815.56, u.solMass)))
+        mdelta = mass_concentration.mdelta_to_mdelta(1e14, func, 2500, 500,
+                                                     (0, self._cosmo))    
+        assert(quantity_allclose(mdelta,
+                                 u.Quantity(155627379902287.5, u.solMass)))
+        # extreme cases
+        mdelta = mass_concentration.mdelta_to_mdelta(1e14, func, 199, 200,
+                                                     (1, self._cosmo))
+        assert(quantity_allclose(mdelta,
+                                 u.Quantity(99840165385091.89, u.solMass)))
+        mdelta = mass_concentration.mdelta_to_mdelta(1e14, func, 200, 200,
+                                                     (1, self._cosmo))
+        assert_equal(mdelta.value, 1e14)
+        mdelta = mass_concentration.mdelta_to_mdelta(1e15, func, 2500, 50,
+                                                     (0, self._cosmo))
+        assert(quantity_allclose(mdelta,
+                                 u.Quantity(6297248192424681.0, u.solMass)))
+        mdelta = mass_concentration.mdelta_to_mdelta(1e9, func, 50, 2500,
+                                                     (1, self._cosmo))
+        assert(quantity_allclose(mdelta,
+                                 u.Quantity(581928261.3835365, u.solMass)))
+        # Consistency
+        z = 0.3
+        m_in = u.Quantity(5e14, u.solMass)
+        mdelta = mass_concentration.mdelta_to_mdelta(5e14, func, 500, 200,
+                                                     (z, self._cosmo))
+        c = func(mdelta, z, self._cosmo)
+        nfw = NFW(mdelta, c, z)
+        m_out = nfw.mass_Delta(500)
+        assert(quantity_allclose(m_in, m_out))
