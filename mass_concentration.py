92423efc9df43b62041160e3f12650755d52cfcb
Jörg Dietrich <astro@joergdietrich.com>
2016-03-14 14:58:40 +0100

    add m200_to_delta function, use it it to fix mdelta_to_mdelta

--- mass_concentration.py
+++ mass_concentration.py
@@ -10,25 +10,39 @@ from scipy import optimize as opt
 from astropy import units as u
 from astropy import cosmology
 
+
 def _diff_c(c1, c0, delta_ratio):
     return _delta_fac(c0) / _delta_fac(c1) - delta_ratio * (c0 / c1)**3
 
-def _findc(c0, overdensity_in, overdensity_out):
-    delta_ratio = overdensity_out / overdensity_in    
+
+def _findc(c0, overdensity):
+    delta_ratio = 200 / overdensity
     return opt.brentq(_diff_c, .01, 1000, args=(c0, delta_ratio))
+
 
 def _delta_fac(c):
     return np.log(1 + c) - c / (1 + c)
 
+
 def _find_m200(m200, *args):
     func = args[0]
-    overdensity_in = args[1]
-    overdensity_out = args[2]
-    m_in = args[3]
-    my_args = args[4:]
+    overdensity = args[1]
+    m_in = args[2]
+    my_args = args[3:]
     c0 = func(m200, *my_args)
-    c1 = _findc(c0, overdensity_in, overdensity_out)
+    c1 = _findc(c0, overdensity)
     return m200 / m_in.value - _delta_fac(c0) / _delta_fac(c1)
+
+
+def _find_mdelta(mdelta, *args):
+    func = args[0]
+    overdensity = args[1]
+    m_in = args[2]
+    my_args = args[3:]
+    c200 = func(m_in, *my_args)
+    c1 = _findc(c200, overdensity)
+    return m_in.value / mdelta - _delta_fac(c200) / _delta_fac(c1)
+    pass
 
 
 def mdelta_to_mdelta(m, func, overdensity_in, overdensity_out, args=()):
@@ -36,6 +50,7 @@ def mdelta_to_mdelta(m, func, overdensity_in, overdensity_out, args=()):
     Convert the a mass given in a variable overdensity with respect to
     the critical density to the mass in another overdensity wrt the
     critical density following a fixed mass concentration relation.
+
 
     Parameters:
     ===========
@@ -59,22 +74,16 @@ def mdelta_to_mdelta(m, func, overdensity_in, overdensity_out, args=()):
     ====== 
     Halo masses must be given in units expected by the M-c relation.
     """
-    m_in = u.Quantity(m, u.solMass)
-    if overdensity_in == overdensity_out:
-        # brentq will fail for identical
-        return m_in
-    delta_ratio = overdensity_in / overdensity_out
-    m_min = u.Quantity(1e5, u.solMass)
-    m_max = u.Quantity(1e25, u.solMass)
-    mdelta = opt.brentq(_find_m200, m_min.value, m_max.value,
-                       args=(func, overdensity_in, overdensity_out, m_in) \
-                        + args)
+    m200 = mdelta_to_m200(m, func, overdensity_in, args)
+    mdelta = m200_to_mdelta(m200, func, overdensity_out, args)
     return u.Quantity(mdelta, u.solMass)
 
 
 def mdelta_to_m200(m, func, overdensity, args=()):
     """
-    Convenience function for mdelta_to_mdelta with output overdensity 200.
+    Convert the a mass given in a variable overdensity with respect to
+    the critical density to the mass in 200 times overdensity wrt the
+    critical density following a fixed mass concentration relation.
 
     Parameters:
     ===========
@@ -96,7 +105,53 @@ def mdelta_to_m200(m, func, overdensity, args=()):
     ======
     Halo masses must be given in units expected by the M-c relation.
     """
-    return mdelta_to_mdelta(m, func, overdensity, 200, args)
+    m_in = u.Quantity(m, u.solMass)
+    if overdensity == 200:
+        # brentq will fail for identical
+        return m_in
+    m_min = u.Quantity(1e5, u.solMass)
+    m_max = u.Quantity(1e25, u.solMass)
+    mdelta = opt.brentq(_find_m200, m_min.value, m_max.value,
+                       args=(func, overdensity, m_in) + args)
+    return u.Quantity(mdelta, u.solMass)
+
+
+def m200_to_mdelta(m, func, overdensity, args=()):
+    """
+    Convert the a mass given in 200 times the critical overdensity to
+    the mass in another overdensity wrt the critical density following
+    a fixed mass concentration relation.
+
+    Parameters:
+    ===========
+    m: float or astropy.Quantity
+        mass of halo
+    func: callable func(m200c, *args)
+        Mass-concentration scaling relation
+    overdensity: float
+        Overdensity in units of rho_crit at which halo mass is desired
+    args: tuple, optional
+        Extra arguments passed to func, i.e., ``f(x, *args)``.
+
+    Returns:
+    ========
+    m200: astropy.Quantity
+        mass of halo at 200 times critical overdensity
+
+    Notes:
+    ======
+    Halo masses must be given in units expected by the M-c relation.
+
+    """
+    m_in = u.Quantity(m, u.solMass)
+    if overdensity == 200:
+        # brentq will fail for identical
+        return m_in
+    m_min = u.Quantity(1e5, u.solMass)
+    m_max = u.Quantity(1e25, u.solMass)
+    mdelta = opt.brentq(_find_mdelta, m_min.value, m_max.value,
+                       args=(func, overdensity, m_in) + args)
+    return u.Quantity(mdelta, u.solMass)
 
 
 def dolag_concentration(m200, z, cosmo):
@@ -129,7 +184,7 @@ def dolag_concentration(m200, z, cosmo):
     conc = 9.59 / (1 + z) * (m200 / 1e14)**-0.102
     return conc
 
-  
+
 def duffy_concentration(m200, z, cosmo):
     """
     Compute the concentration of the Duffy et al. (2008)
@@ -164,4 +219,3 @@ def duffy_concentration(m200, z, cosmo):
     m_pivot = u.Quantity(2e12, u.solMass)
     conc = a * (m200 / m_pivot)**b * (1 + z)**c
     return conc.value
-
