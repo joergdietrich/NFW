dd418ec5dc183380bc39c77ba5027935b2c02be4
Jörg Dietrich <astro@joergdietrich.com>
2018-02-21 18:01:41 +0100

    consistent upper mass limit in mdelta conversion

--- NFW/mass_concentration.py
+++ NFW/mass_concentration.py
@@ -145,7 +145,7 @@ def m200_to_mdelta(m, func, overdensity, args=()):
         # brentq will fail for identical
         return m_in
     m_min = u.Quantity(1e5, u.solMass)
-    m_max = u.Quantity(1e25, u.solMass)
+    m_max = u.Quantity(1e20, u.solMass)
     mdelta = opt.brentq(_find_mdelta, m_min.value, m_max.value,
                         args=(func, overdensity, m_in) + args)
     return u.Quantity(mdelta, u.solMass)
